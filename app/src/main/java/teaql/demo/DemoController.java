de0709dac1fe21ad08554a1d0153235c7324fc97
Philip Zhang <zhangxilai@doublechaintech.com>
2023-05-12 12:54:56 +0800

    🚀update version

--- app/src/main/java/teaql/demo/DemoController.java
+++ app/src/main/java/teaql/demo/DemoController.java
@@ -1,8 +1,9 @@
 package teaql.demo;
 
 import cn.hutool.core.map.MapUtil;
-import com.testbank.bank.BankUserContext;
-import com.testbank.bank.Q;
+import com.doublechaintech.todo.TodoUserContext;
+import com.doublechaintech.todo.merchant.Merchant;
+import com.doublechaintech.todo.todoitem.TodoItem;
 import io.teaql.data.TQLContext;
 import io.teaql.data.UserContext;
 import io.teaql.data.meta.EntityMetaFactory;
@@ -10,7 +11,9 @@ import io.teaql.data.sql.SQLRepositorySchemaHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import com.doublechaintech.todo.Q;
 
 @Controller
 public class DemoController {
@@ -23,10 +26,44 @@ public class DemoController {
     new SQLRepositorySchemaHelper().ensureSchema(context, factory);
     return MapUtil.of("ok", true);
   }
-
+  
   @GetMapping("/search")
   @ResponseBody
-  public Object query(@TQLContext BankUserContext context) throws Exception {
-    return Q.merchantStatus().selectPlatform().executeForList(context);
+  public Object query(@TQLContext TodoUserContext context)  {
+
+
+
+    return Q.merchant().selectPlatform().executeForList(context);
   }
+  @GetMapping("/search2")
+  @ResponseBody
+  public Object query2(@TQLContext TodoUserContext context) {
+    return Q.todoItem().executeForList(context);
+  }
+
+  @GetMapping("/new/{name}/")
+  @ResponseBody
+  public Object newTodo(@TQLContext TodoUserContext context,@PathVariable("name") String name)
+          throws Exception {
+
+
+    TodoItem todoItem=new TodoItem().updateName(name);
+    todoItem.updateStatus("TODO");
+    todoItem.updateMerchant(Q.merchant().executeForList(context).first());
+    return todoItem.save(context);
+
+  }
+  @GetMapping("/get/")
+  @ResponseBody
+  public Object get(@TQLContext TodoUserContext context) {
+
+
+
+
+    return Q.merchant().execute(context);
+
+  }
+
+
+
 }
